546675e2b28d8dffb4f897be9da3ce80f1fd73d7
Plushcake <Nikkolo.Makeawelli@gmail.com>
2022-09-13 19:17:55 +0300

    Modified v.2.0.1

--- src/test/java/test/AuthTest.java
+++ src/test/java/test/AuthTest.java
@@ -1,9 +1,12 @@
 package test;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -14,7 +17,8 @@ import static data.DataGenerator.getRandomPassword;
 
 public class AuthTest {
 
-    @BeforeEach //Выполняется перед каждым тестом.
+    @BeforeEach
+        //Выполняется перед каждым тестом.
     void setup() {
         open("http://localhost:9999");
         Configuration.holdBrowserOpen = true;
@@ -32,6 +36,8 @@ public class AuthTest {
         $("[data-test-id=\"password\"] input").setValue(registeredUser.getPassword());
         $("[data-test-id=\"action-login\"]").click();
 
+        $("[id=\"root\"]").shouldHave(Condition.text("Личный кабинет"), Duration.ofSeconds(15));
+
     }
 
     @Test
@@ -40,6 +46,11 @@ public class AuthTest {
         var notRegisteredUser = getUser("active");
         // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет
         //  незарегистрированного пользователя, для заполнения полей формы используйте пользователя notRegisteredUser
+        $("[data-test-id=\"login\"] input").setValue(notRegisteredUser.getLogin());
+        $("[data-test-id=\"password\"] input").setValue(notRegisteredUser.getPassword());
+        $("[data-test-id=\"action-login\"]").click();
+
+        $("[data-test-id=\"error-notification\"]").shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"), Duration.ofSeconds(15));
     }
 
     @Test
@@ -48,6 +59,11 @@ public class AuthTest {
         var blockedUser = getRegisteredUser("blocked");
         // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет,
         //  заблокированного пользователя, для заполнения полей формы используйте пользователя blockedUser
+        $("[data-test-id=\"login\"] input").setValue(blockedUser.getLogin());
+        $("[data-test-id=\"password\"] input").setValue(blockedUser.getPassword());
+        $("[data-test-id=\"action-login\"]").click();
+
+        $("[data-test-id=\"error-notification\"]").shouldHave(Condition.text("Ошибка! Пользователь заблокирован"), Duration.ofSeconds(15));
     }
 
     @Test
@@ -58,6 +74,11 @@ public class AuthTest {
         // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет с неверным
         //  логином, для заполнения поля формы "Логин" используйте переменную wrongLogin,
         //  "Пароль" - пользователя registeredUser
+        $("[data-test-id=\"login\"] input").setValue(wrongLogin);
+        $("[data-test-id=\"password\"] input").setValue(registeredUser.getPassword());
+        $("[data-test-id=\"action-login\"]").click();
+
+        $("[data-test-id=\"error-notification\"]").shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"));
     }
 
     @Test
@@ -68,6 +89,11 @@ public class AuthTest {
         // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет с неверным
         //  паролем, для заполнения поля формы "Логин" используйте пользователя registeredUser,
         //  "Пароль" - переменную wrongPassword
+        $("[data-test-id=\"login\"] input").setValue(registeredUser.getLogin());
+        $("[data-test-id=\"password\"] input").setValue(wrongPassword);
+        $("[data-test-id=\"action-login\"]").click();
+
+        $("[data-test-id=\"error-notification\"]").shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"));
     }
 }
 
